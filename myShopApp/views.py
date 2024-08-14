745a5b245ceb95e991063b5aa8e1cde205821435
hundalgkaur <125379419+hundalgkaur@users.noreply.github.com>
2024-08-14 22:18:06 +0530

    add product

--- myShopApp/views.py
+++ myShopApp/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from myShopApp.models import Contact,Product
+from django.contrib import messages
+from math import ceil
+
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    print(catprods)
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod= Product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params= {'allProds':allProds}
+
+    return render(request,"index.html",params)
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email =request.POST.get("email")
+        desc = request.POST.get("desc")
+        phonenumber = request.POST.get("phonenumber")
+        myquery = Contact(name=name,email=email,desc=desc,phonenumber=phonenumber)
+        myquery.save()
+        messages.info(request,"we will get back to you soon ")
+        return render(request,"contact.html")
+
     return render(request,"contact.html")
 def about(request):
     return render(request,"about.html")
