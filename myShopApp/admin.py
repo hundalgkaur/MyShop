1ca9e3335488c9b0906201eb96e268884effcea1
hundalgkaur <125379419+hundalgkaur@users.noreply.github.com>
2024-08-16 20:46:48 +0530

    cart page

--- myShopApp/admin.py
+++ myShopApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from myShopApp.models import Contact,Product
+from myShopApp.models import Contact,Product,Orders,OrderUpdate
 
 
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
